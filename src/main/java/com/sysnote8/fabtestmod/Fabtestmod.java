7dd58d345a9eb17c7a2d0907e485f2b088ff68a3
sysnote8main <69232601+sysnote8main@users.noreply.github.com>
2023-10-02 20:00:33 +0900

    initalize commit

---
+++ src/main/java/com/sysnote8/fabtestmod/Fabtestmod.java
@@ -0,0 +1,13 @@
+package com.sysnote8.fabtestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fabtestmod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
